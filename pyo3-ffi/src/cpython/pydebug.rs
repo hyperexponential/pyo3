56b2227fb88b03ffb631ea495a6fe65f122b4f08
Peter Hall <peter.hall@hyperexponential.com>
2024-02-16 18:27:15 +0000

    Add python version feature gate to debug flag deprecations

--- pyo3-ffi/src/cpython/pydebug.rs
+++ pyo3-ffi/src/cpython/pydebug.rs
@@ -3,55 +3,55 @@ use std::os::raw::{c_char, c_int};
 #[cfg(not(Py_LIMITED_API))]
 #[cfg_attr(windows, link(name = "pythonXY"))]
 extern "C" {
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_DebugFlag")]
     pub static mut Py_DebugFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_VerboseFlag")]
     pub static mut Py_VerboseFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     pub static mut Py_QuietFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_InteractiveFlag")]
     pub static mut Py_InteractiveFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_InspectFlag")]
     pub static mut Py_InspectFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_OptimizeFlag")]
     pub static mut Py_OptimizeFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_NoSiteFlag")]
     pub static mut Py_NoSiteFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_BytesWarningFlag")]
     pub static mut Py_BytesWarningFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_UseClassExceptionsFlag")]
     pub static mut Py_UseClassExceptionsFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_FrozenFlag")]
     pub static mut Py_FrozenFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_IgnoreEnvironmentFlag")]
     pub static mut Py_IgnoreEnvironmentFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_DontWriteBytecodeFlag")]
     pub static mut Py_DontWriteBytecodeFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     #[cfg_attr(PyPy, link_name = "PyPy_NoUserSiteDirectory")]
     pub static mut Py_NoUserSiteDirectory: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     pub static mut Py_UnbufferedStdioFlag: c_int;
     #[cfg_attr(PyPy, link_name = "PyPy_HashRandomizationFlag")]
     pub static mut Py_HashRandomizationFlag: c_int;
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     pub static mut Py_IsolatedFlag: c_int;
     #[cfg(windows)]
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     pub static mut Py_LegacyWindowsFSEncodingFlag: c_int;
     #[cfg(windows)]
-    #[deprecated(note = "Python 3.12")]
+    #[cfg_attr(Py_3_12, deprecated(note = "Python 3.12"))]
     pub static mut Py_LegacyWindowsStdioFlag: c_int;
 }
 
